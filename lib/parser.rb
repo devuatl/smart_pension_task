3760626eb151d9b89da4db03aa6724a1413d469d
Lukasz Kieda <lukasz.kieda@u2i.com>
2019-04-27 10:45:52 +0200

    Add support for counting unique views. Refactor specs.

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,24 +1,33 @@
 class Parser
-  def self.parse(file_name)
-    # File.read opens and closes the file
+  def self.parse(file_name, aggregation_type: :visits)
     log_lines = read_lines_from_file(file_name)
-    aggregated_paths = aggregate_paths(log_lines)
-    print_aggregated_paths(aggregated_paths)
+    log_lines.uniq! if aggregation_type == :unique_views
+
+    paths = paths_from_logs(log_lines)
+    occurrences = count_occurrences(paths)
+    sorted_occurrences = sort_descending_by_value(occurrences)
+
+    print_occurrences(sorted_occurrences, aggregation_type)
+  end
+
+  def self.print_occurrences(sorted_occurrences, aggregation_type)
+    aggregation_label = aggregation_type.to_s.gsub('_', ' ')
+    sorted_occurrences.each {|path, count| puts "#{path} #{count} #{aggregation_label}"}
+  end
+
+  def self.paths_from_logs(log_lines)
+    log_lines.map{|line| line.split(' ').first }
+  end
+
+  def self.sort_descending_by_value(occurrences)
+    occurrences.sort_by { |_key, value| value }.reverse
+  end
+
+  def self.count_occurrences(paths)
+    paths.inject(Hash.new(0)) { |acc, path| acc[path] = acc[path] + 1; acc }
   end
 
   def self.read_lines_from_file(file_name)
-    # What if file name is empty?
-    # What if file does not exist?
     File.read(file_name).split("\n")
-  end
-
-  def self.aggregate_paths(log_lines)
-    paths = log_lines.map{|line| line.split(' ').first }
-    path_views = paths.inject(Hash.new(0)) { |acc, path| acc[path] = acc[path] + 1; acc }
-    path_views.sort_by { |_key, value| value }.reverse # path_views_descending
-  end
-
-  def self.print_aggregated_paths(aggregated_paths)
-    aggregated_paths.each {|path, views| puts "#{path} #{views} visits"}
   end
 end
--- spec/lib/parser_spec.rb
+++ spec/lib/parser_spec.rb
@@ -1,20 +1,43 @@
 require 'parser'
 
 RSpec.describe Parser do
-  context 'read lines from logs file' do
-    let(:visits_output) do
-      File.read('spec/fixtures/visits_output.txt')
+  context 'reads lines from log file' do
+    let(:log_file) {'spec/fixtures/example_input.log'}
+    let(:visits_output) {File.read('spec/fixtures/visits_output.txt')}
+    let(:unique_views_output) {File.read('spec/fixtures/unique_views_output.txt')}
+
+    subject(:parse) { described_class.parse(log_file, aggregation_type: aggregation_type) }
+
+    context 'when aggregation type is visits' do
+      let(:aggregation_type) { :visits }
+      it 'should output ordered number of visits' do
+        expect { parse }.to output(visits_output).to_stdout
+      end
     end
 
-    it 'prints aggregated paths' do
-      expect { described_class.parse('spec/fixtures/example_input.log') }.to output(visits_output).to_stdout
+    context 'when aggregation type is unique views' do
+      let(:aggregation_type) { :unique_views }
+      it 'should output ordered number of unique views' do
+        expect { parse }.to output(unique_views_output).to_stdout
+      end
     end
   end
 
-  context '.aggregate_paths' do
-    it 'returns empty array when input is an empty array' do
-      aggregated_paths = Parser.aggregate_paths([])
-      expect(aggregated_paths).to eq([])
+  context '.count_path_occurrences' do
+    subject(:count_path_occurrences){ described_class.count_occurrences(paths) }
+
+    context 'when paths are an empty array' do
+      let(:paths) { [] }
+      let(:result) { {} }
+
+      it { expect(count_path_occurrences).to eq(result) }
+    end
+
+    context 'when paths are present' do
+      let(:paths) { ['/help_page/1', '/help_page/1', '/contact'] }
+      let(:result) {{'/help_page/1' => 2, '/contact' => 1}}
+
+      it { expect(count_path_occurrences).to eq(result) }
     end
   end
 end
